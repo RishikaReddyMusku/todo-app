ed069645f413801ae6136f26ad79290f2da67f59
RishikaReddyMusku <rishikamusku20gmail.com>
2025-04-09 19:36:07 +0530

    fixed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,5 @@
-const API_URL = https://todo-app-vn29.onrender.com;
+const API_URL = "https://todo-app-vn29.onrender.com/api";
 
-fetch(`${API_URL}/api/todos`);
+fetch(`${API_URL}/todos`)  // ✅ Correct path
+  .then(res => res.json())
+  .then(data => console.log(data));
